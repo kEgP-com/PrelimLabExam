5b74a8abbb273b6fce5d3f33c9993f3a829b96a2
kiteCodePro <128288912+kiteCodePro@users.noreply.github.com>
2025-09-25 15:39:09 +0800

    update the values of the books and its ISBN

--- setup.php
+++ setup.php
@@ -1,53 +1,43 @@
 <?php
 $conn = new mysqli("db", "root", "rootpassword");
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    die("Can't connect: " . $conn->connect_error);
 }
 
-$sql = "CREATE DATABASE IF NOT EXISTS library_db";
-if (!$conn->query($sql)) {
-    die("Error creating database: " . $conn->error);
-}
-
+$conn->query("CREATE DATABASE IF NOT EXISTS library_db");
 $conn->select_db("library_db");
 
+
 // default login table the credentials such as the user and the librarian
-$sql = "CREATE TABLE IF NOT EXISTS users (
+$conn->query("CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) UNIQUE,
     password VARCHAR(255),
     role ENUM('user','librarian')
-)";
-if (!$conn->query($sql)) {
-    die("Error creating users table: " . $conn->error);
-}
+)");
 
-$conn->query("INSERT IGNORE INTO users (id, username, password, role) VALUES
-    (1, 'admin', MD5('1234'), 'librarian'),
-    (2, 'user1', MD5('1234'), 'user')
+$conn->query("INSERT IGNORE INTO users (username, password, role) VALUES
+    ('admin', MD5('1234'), 'librarian'),
+    ('user1', MD5('1234'), 'user')
 )");
 
 
-$sql = "CREATE TABLE IF NOT EXISTS books (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+$conn->query("CREATE TABLE IF NOT EXISTS books (
+    isbn VARCHAR(20) PRIMARY KEY,
     title VARCHAR(100) NOT NULL,
     author VARCHAR(100) NOT NULL,
     copies INT DEFAULT 1,
     available INT DEFAULT 1,
     date_added DATE
-)";
-if (!$conn->query($sql)) {
-    die("Error creating books table: " . $conn->error);
-}
-
-
-$conn->query("INSERT IGNORE INTO books (id, title, author, copies, available, date_added) VALUES
-    (1, 'The Great Gatsby', 'F. Scott Fitzgerald', 3, 3, '2025-01-10'),
-    (2, '1984', 'George Orwell', 5, 5, '2025-02-15'),
-    (3, 'To Kill a Mockingbird', 'Harper Lee', 4, 4, '2025-03-20'),
-    (4, 'Pride and Prejudice', 'Jane Austen', 2, 2, '2025-04-25'),
-    (5, 'The Catcher in the Rye', 'J.D. Salinger', 3, 3, '2025-05-30')
 )");
 
-echo "Database and tables created successfully, default users and books added.";
+$conn->query("INSERT IGNORE INTO books (isbn, title, author, copies, available, date_added) VALUES
+    ('B001', 'The Great Gatsby', 'F. Scott Fitzgerald', 3, 3, '2025-01-10'),
+    ('B002', '1984', 'George Orwell', 15, 5, '2025-02-15'),
+    ('B003', 'To Kill a Mockingbird', 'Harper Lee', 4, 4, '2025-03-20'),
+    ('B004', 'Pride and Prejudice', 'Jane Austen', 13, 2, '2025-04-25'),
+    ('B005', 'Salimsim', 'Binibining Mia', 3, 3, '2025-05-30')
+)");
+
+echo "Setup done. Users and books added.";
 ?>
